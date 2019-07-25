e75b73291fceee78df28211e32054fd89ff5eee5
LennartDo <lennart.domdei@gmail.com>
2019-07-25 15:39:29 +0200

    first commit

---
+++ VideoProcessor/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace VideoProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInputDir = new System.Windows.Forms.TextBox();
+            this.btInputDirDialog = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbOutputDir = new System.Windows.Forms.TextBox();
+            this.btOutputDirDialog = new System.Windows.Forms.Button();
+            this.btStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Directory:";
+            // 
+            // tbInputDir
+            // 
+            this.tbInputDir.Location = new System.Drawing.Point(28, 47);
+            this.tbInputDir.Name = "tbInputDir";
+            this.tbInputDir.Size = new System.Drawing.Size(240, 20);
+            this.tbInputDir.TabIndex = 1;
+            // 
+            // btInputDirDialog
+            // 
+            this.btInputDirDialog.Location = new System.Drawing.Point(274, 47);
+            this.btInputDirDialog.Name = "btInputDirDialog";
+            this.btInputDirDialog.Size = new System.Drawing.Size(27, 20);
+            this.btInputDirDialog.TabIndex = 2;
+            this.btInputDirDialog.Text = "...";
+            this.btInputDirDialog.UseVisualStyleBackColor = true;
+            this.btInputDirDialog.Click += new System.EventHandler(this.btInputDirDialog_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output Directory:";
+            // 
+            // tbOutputDir
+            // 
+            this.tbOutputDir.Location = new System.Drawing.Point(28, 107);
+            this.tbOutputDir.Name = "tbOutputDir";
+            this.tbOutputDir.Size = new System.Drawing.Size(240, 20);
+            this.tbOutputDir.TabIndex = 4;
+            // 
+            // btOutputDirDialog
+            // 
+            this.btOutputDirDialog.Location = new System.Drawing.Point(274, 107);
+            this.btOutputDirDialog.Name = "btOutputDirDialog";
+            this.btOutputDirDialog.Size = new System.Drawing.Size(27, 20);
+            this.btOutputDirDialog.TabIndex = 5;
+            this.btOutputDirDialog.Text = "...";
+            this.btOutputDirDialog.UseVisualStyleBackColor = true;
+            this.btOutputDirDialog.Click += new System.EventHandler(this.btOutputDirDialog_Click);
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(26, 155);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(275, 23);
+            this.btStart.TabIndex = 6;
+            this.btStart.Text = "Process Folder";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 200);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.btOutputDirDialog);
+            this.Controls.Add(this.tbOutputDir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btInputDirDialog);
+            this.Controls.Add(this.tbInputDir);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Video Processor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbInputDir;
+        private System.Windows.Forms.Button btInputDirDialog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbOutputDir;
+        private System.Windows.Forms.Button btOutputDirDialog;
+        private System.Windows.Forms.Button btStart;
+    }
+}
+
---
+++ VideoProcessor/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoProcessor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tbInputDir.AppendText("C:\\Users\\Lennart\\Desktop\\Lennart\\Masterarbeit\\Videos\\BAK8013\\7_23_2019_11_32_37");
+            tbOutputDir.AppendText("C:\\Users\\Lennart\\Desktop\\Lennart\\Masterarbeit\\Videos");
+        }
+
+        private void btInputDirDialog_Click(object sender, EventArgs e)
+        {
+            using (var Dialog = new FolderBrowserDialog())
+            {
+                DialogResult Result = Dialog.ShowDialog();
+                if (Result == DialogResult.OK && !string.IsNullOrWhiteSpace(Dialog.SelectedPath))
+                {
+                    if (Dialog.SelectedPath[Dialog.SelectedPath.Length-1] != '\\')
+                        Dialog.SelectedPath += "\\";
+
+                    tbInputDir.Text = Dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void btOutputDirDialog_Click(object sender, EventArgs e)
+        {
+            using (var Dialog = new FolderBrowserDialog())
+            {
+                DialogResult Result = Dialog.ShowDialog();
+
+                if (Result == DialogResult.OK && !string.IsNullOrWhiteSpace(Dialog.SelectedPath))
+                {
+                    if (Dialog.SelectedPath[Dialog.SelectedPath.Length-1] != '\\')
+                        Dialog.SelectedPath += "\\";
+
+                    tbOutputDir.Text = Dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            string InputPath = tbInputDir.Text;
+            string OutputPath = tbOutputDir.Text;
+            if (!OutputPath.EndsWith("\\"))
+                OutputPath += "\\";
+
+            if (System.IO.Directory.Exists(InputPath) && System.IO.Directory.Exists(OutputPath))
+            {
+                string[] FilePaths = System.IO.Directory.GetFiles(InputPath);
+
+                for (int i = 0; i < FilePaths.Length; ++i)
+                {
+                    if (FilePaths[i].EndsWith(".avi"))
+                    {
+                        string FileNameWithoutPath = System.IO.Path.GetFileNameWithoutExtension(FilePaths[i]);
+                        string FileCompleteTop = OutputPath + FileNameWithoutPath + "_top" + System.IO.Path.GetExtension(FilePaths[i]);
+                        string FileCompleteBottom = OutputPath + FileNameWithoutPath + "_bottom" + System.IO.Path.GetExtension(FilePaths[i]);
+
+                        ProcessTop(FilePaths[i], FileCompleteTop);
+                        ProcessBottom(FilePaths[i], FileCompleteBottom);
+                    }
+                }
+
+                btStart.Text = "Done!";
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid directory paths.");
+            }
+        }
+
+        private void ProcessTop(string FileInput, string FileOutput)
+        {
+            using (Process p = new Process())
+            {
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.FileName = "ffmpeg.exe";                             // width:height:xoffset:yoffset           ohne: -codec libx264 -preset ultrafast -crf 0 für windows Mediaplayer
+                p.StartInfo.Arguments = "-i " + FileInput + " -filter_complex \"[0:v]crop=512:250:0:6[vid]; [vid]scale=512:512\" -codec libx264 -preset ultrafast -crf 0 " + FileOutput;
+                                                                                                            // width : height
+                p.Start();
+                p.WaitForExit();
+            }
+        }
+
+        private void ProcessBottom(string FileInput, string FileOutput)
+        {
+            using (Process p = new Process())
+            {
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.FileName = "ffmpeg.exe";                               // width:height:xoffset:yoffset           ohne: -codec libx264 -preset ultrafast -crf 0 für windows Mediaplayer
+                p.StartInfo.Arguments = "-i " + FileInput + " -filter_complex \"[0:v]crop=512:256:0:256[vid]; [vid]scale=512:512\" -codec libx264 -preset ultrafast -crf 0 " + FileOutput;
+                                                                                                            // width : height
+                p.Start();
+                p.WaitForExit();
+            }
+        }
+    }
+}
